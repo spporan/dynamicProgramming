e4be05f972c0630fd6d2b7ffc70ed291f67e371c
poran <poran.cse@gmail.com>
2022-08-06 12:54:18 +0600

    calculated time complexity and documented

--- src/main/kotlin/Fibonacci.kt
+++ src/main/kotlin/Fibonacci.kt
@@ -4,7 +4,28 @@ fun main() {
     println("fib of 50 ${fib(50)}")
 }
 
-//fibonacci number by dynamic programming
+
+/**
+ * fibonacci number solved by brute force technique
+ *
+ * time complexity O (2^n) ###  where n is the level of tree, every root node have 2 two child
+ *  array.
+ *
+ *  space complexity O(n)  ### where n is the level of the tree
+ */
+fun fibWithBruteForce(n: Long): Long {
+    if (n <= 2) return 1
+    return fib(n-1) + fib(n-2)
+}
+
+/**
+ * fibonacci number solved by memorizing technique
+ *
+ *  time complexity O (n) ###  where n is the level of tree
+ *  array.
+ *
+ *  space complexity O(n)  ### where n is the level of the tree
+ */
 fun fib(n: Long, map: HashMap<Long, Long> = hashMapOf<Long, Long>()): Long {
     if (map.containsKey(n)) return map.getValue(n)
 
